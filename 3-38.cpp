51d860d6d33a66dcbbc0db2a8a0daf2fe99e7917
nanami-tomoe <npnp123npnp123@gmail.com>
2023-03-30 02:04:09 +0900

    실습 3 - 38번

---
+++ 3-38.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+const int SIZE = 10; // 배열 크기
+void PrintArray(int a[], int size){
+  for(int i = 0; i < size; i++) cout << a[i] << " ";
+  cout << endl;
+}
+
+int main(){
+  int a[SIZE] = {1, 2, 2, 3, 4, 5, 2, 7, 3, 9};
+  int size = SIZE;
+  int i, j, num1 = 2, num2 = 3;
+  cout << "before: ";
+  PrintArray(a, size);
+
+  for(i = 0, j = 0; i < size; i++){
+    if(a[i] != num1 && a[i] != num2) a[j++] = a[i];
+  }
+  size = j;
+  cout << "after: ";
+  PrintArray(a, size);
+
+}
